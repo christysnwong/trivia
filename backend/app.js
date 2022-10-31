08feda1f0e677e49fd7600d8d6d71f3885ac6291
Christy <christy.sn.wong@gmail.com>
2022-10-31 11:19:58 -0700

    25th commit - fixed app.js

--- backend/app.js
+++ backend/app.js
@@ -43,7 +43,7 @@ app.use(function (req, res, next) {
 /** Generic error handler; anything unhandled goes here. */
 app.use(function (err, req, res, next) {
   if (process.env.NODE_ENV !== "test") console.error(err.stack);
-  console.error(json.stringify())
+
   const status = err.status || 500;
   const message = err.message;
 
